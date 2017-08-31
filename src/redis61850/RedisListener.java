a85414991bf4ccca53ffc5cb30cbbcc21f9806d5
Daniele Pala <pala.daniele@gmail.com>
2017-08-31 11:37:27 +0200

    Add license information.

--- src/redis61850/RedisListener.java
+++ src/redis61850/RedisListener.java
@@ -1,3 +1,24 @@
+/*
+ Code for connecting Redis and OpenIEC61850.
+
+ Copyright 2017 Daniele Pala <daniele.pala@rse-web.it>
+
+ This file is part of openIEC61850-redis.
+
+ openIEC61850-redis is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ openIEC61850-redis is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with openIEC61850-redis. If not, see <http://www.gnu.org/licenses/>.
+*/
+
 package redis61850;
 
 import java.io.BufferedReader;
